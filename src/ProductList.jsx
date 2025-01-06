03f9c2c552fbf6831fb50bd0e854982dccad6605
Nookendra-23 <vijaybotcha40@gmail.com>
2025-01-06 21:03:55 +0530

    Deploying React app to GitHub Pages

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -32,37 +32,37 @@ function ProductList(props) {
                     name: "Snake Plant",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
                     description: "Produces oxygen at night, improving air quality.",
-                    cost: "Rs.115"
+                    cost: "$15"
                 },
                 {
                     name: "Spider Plant",
                     image: "https://cdn.pixabay.com/photo/2018/07/11/06/47/chlorophytum-3530413_1280.jpg",
                     description: "Filters formaldehyde and xylene from the air.",
-                    cost: "Rs.112"
+                    cost: "$12"
                 },
                 {
                     name: "Peace Lily",
                     image: "https://cdn.pixabay.com/photo/2019/06/12/14/14/peace-lilies-4269365_1280.jpg",
                     description: "Removes mold spores and purifies the air.",
-                    cost: "Rs.118"
+                    cost: "$18"
                 },
                 {
                     name: "Boston Fern",
                     image: "https://cdn.pixabay.com/photo/2020/04/30/19/52/boston-fern-5114414_1280.jpg",
                     description: "Adds humidity to the air and removes toxins.",
-                    cost: "Rs.120"
+                    cost: "$20"
                 },
                 {
                     name: "Rubber Plant",
                     image: "https://cdn.pixabay.com/photo/2020/02/15/11/49/flower-4850729_1280.jpg",
                     description: "Easy to care for and effective at removing toxins.",
-                    cost: "Rs.117"
+                    cost: "$17"
                 },
                 {
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
                     description: "Purifies the air and has healing properties for skin.",
-                    cost: "Rs.114"
+                    cost: "$14"
                 }
             ]
         },
@@ -73,37 +73,37 @@ function ProductList(props) {
                     name: "Lavender",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Calming scent, used in aromatherapy.",
-                    cost: "Rs.120"
+                    cost: "$20"
                 },
                 {
                     name: "Jasmine",
                     image: "https://images.unsplash.com/photo-1592729645009-b96d1e63d14b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Sweet fragrance, promotes relaxation.",
-                    cost: "Rs.118"
+                    cost: "$18"
                 },
                 {
                     name: "Rosemary",
                     image: "https://cdn.pixabay.com/photo/2019/10/11/07/12/rosemary-4541241_1280.jpg",
                     description: "Invigorating scent, often used in cooking.",
-                    cost: "Rs.115"
+                    cost: "$15"
                 },
                 {
                     name: "Mint",
                     image: "https://cdn.pixabay.com/photo/2016/01/07/18/16/mint-1126282_1280.jpg",
                     description: "Refreshing aroma, used in teas and cooking.",
-                    cost: "Rs.112"
+                    cost: "$12"
                 },
                 {
                     name: "Lemon Balm",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
                     description: "Citrusy scent, relieves stress and promotes sleep.",
-                    cost: "Rs.114"
+                    cost: "$14"
                 },
                 {
                     name: "Hyacinth",
                     image: "https://cdn.pixabay.com/photo/2019/04/07/20/20/hyacinth-4110726_1280.jpg",
                     description: "Hyacinth is a beautiful flowering plant known for its fragrant.",
-                    cost: "Rs.122"
+                    cost: "$22"
                 }
             ]
         },
@@ -114,37 +114,37 @@ function ProductList(props) {
                     name: "oregano",
                     image: "https://cdn.pixabay.com/photo/2015/05/30/21/20/oregano-790702_1280.jpg",
                     description: "The oregano plants contains compounds that can deter certain insects.",
-                    cost: "Rs.110"
+                    cost: "$10"
                 },
                 {
                     name: "Marigold",
                     image:"https://cdn.pixabay.com/photo/2022/02/22/05/45/marigold-7028063_1280.jpg",
                     description: "Natural insect repellent, also adds color to the garden.",
-                    cost: "Rs.18"
+                    cost: "$8"
                 },
                 {
                     name: "Geraniums",
                     image: "https://cdn.pixabay.com/photo/2012/04/26/21/51/flowerpot-43270_1280.jpg",
                     description: "Known for their insect-repelling properties while adding a pleasant scent.",
-                    cost: "Rs.120"
+                    cost: "$20"
                 },
                 {
                     name: "Basil",
                     image: "https://cdn.pixabay.com/photo/2016/07/24/20/48/tulsi-1539181_1280.jpg",
                     description: "Repels flies and mosquitoes, also used in cooking.",
-                    cost: "Rs.19"
+                    cost: "$9"
                 },
                 {
                     name: "Lavender",
                     image: "https://images.unsplash.com/photo-1611909023032-2d6b3134ecba?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Calming scent, used in aromatherapy.",
-                    cost: "Rs.120"
+                    cost: "$20"
                 },
                 {
                     name: "Catnip",
                     image: "https://cdn.pixabay.com/photo/2015/07/02/21/55/cat-829681_1280.jpg",
                     description: "Repels mosquitoes and attracts cats.",
-                    cost: "Rs.113"
+                    cost: "$13"
                 }
             ]
         },
@@ -155,37 +155,37 @@ function ProductList(props) {
                     name: "Aloe Vera",
                     image: "https://cdn.pixabay.com/photo/2018/04/02/07/42/leaf-3283175_1280.jpg",
                     description: "Soothing gel used for skin ailments.",
-                    cost: "Rs.114"
+                    cost: "$14"
                 },
                 {
                     name: "Echinacea",
                     image: "https://cdn.pixabay.com/photo/2014/12/05/03/53/echinacea-557477_1280.jpg",
                     description: "Boosts immune system, helps fight colds.",
-                    cost: "Rs.116"
+                    cost: "$16"
                 },
                 {
                     name: "Peppermint",
                     image: "https://cdn.pixabay.com/photo/2017/07/12/12/23/peppermint-2496773_1280.jpg",
                     description: "Relieves digestive issues and headaches.",
-                    cost: "Rs.113"
+                    cost: "$13"
                 },
                 {
                     name: "Lemon Balm",
                     image: "https://cdn.pixabay.com/photo/2019/09/16/07/41/balm-4480134_1280.jpg",
                     description: "Calms nerves and promotes relaxation.",
-                    cost: "Rs.114"
+                    cost: "$14"
                 },
                 {
                     name: "Chamomile",
                     image: "https://cdn.pixabay.com/photo/2016/08/19/19/48/flowers-1606041_1280.jpg",
                     description: "Soothes anxiety and promotes sleep.",
-                    cost: "Rs.115"
+                    cost: "$15"
                 },
                 {
                     name: "Calendula",
                     image: "https://cdn.pixabay.com/photo/2019/07/15/18/28/flowers-4340127_1280.jpg",
                     description: "Heals wounds and soothes skin irritations.",
-                    cost: "Rs.112"
+                    cost: "$12"
                 }
             ]
         },
@@ -196,37 +196,37 @@ function ProductList(props) {
                     name: "ZZ Plant",
                     image: "https://images.unsplash.com/photo-1632207691143-643e2a9a9361?q=80&w=464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                     description: "Thrives in low light and requires minimal watering.",
-                    cost: "Rs.125"
+                    cost: "$25"
                 },
                 {
                     name: "Pothos",
                     image: "https://cdn.pixabay.com/photo/2018/11/15/10/32/plants-3816945_1280.jpg",
                     description: "Tolerates neglect and can grow in various conditions.",
-                    cost: "Rs.110"
+                    cost: "$10"
                 },
                 {
                     name: "Snake Plant",
                     image: "https://cdn.pixabay.com/photo/2021/01/22/06/04/snake-plant-5939187_1280.jpg",
                     description: "Needs infrequent watering and is resilient to most pests.",
-                    cost: "Rs.115"
+                    cost: "$15"
                 },
                 {
                     name: "Cast Iron Plant",
                     image: "https://cdn.pixabay.com/photo/2017/02/16/18/04/cast-iron-plant-2072008_1280.jpg",
                     description: "Hardy plant that tolerates low light and neglect.",
-                    cost: "Rs.120"
+                    cost: "$20"
                 },
                 {
                     name: "Succulents",
                     image: "https://cdn.pixabay.com/photo/2016/11/21/16/05/cacti-1846147_1280.jpg",
                     description: "Drought-tolerant plants with unique shapes and colors.",
-                    cost: "Rs.118"
+                    cost: "$18"
                 },
                 {
                     name: "Aglaonema",
                     image: "https://cdn.pixabay.com/photo/2014/10/10/04/27/aglaonema-482915_1280.jpg",
                     description: "Requires minimal care and adds color to indoor spaces.",
-                    cost: "Rs.122"
+                    cost: "$22"
                 }
             ]
         }
